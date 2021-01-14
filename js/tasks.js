e0260652380a7a3d039f069821cfa1a0c7ff8a51
Roman <surook@bk.ru>
2021-01-14 14:29:05 +0300

    Задачи для собеседования

---
+++ js/tasks.js
@@ -0,0 +1,12 @@
+console.log([] + false + null + true);
+
+
+
+//alert("qwerty"[1]); //выводит элемент строки с указанным индексом (w);
+
+/* console.log(3 && 0 && 1);
+alert(Boolean(3));
+alert(null || 2 && 3 || 4); */
+
+
+alert(typeof(+"Infinity"));
